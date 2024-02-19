75d3785c1d0b815c47b4ffb9cbf982b68d0ef1c0
AlexLenk <a.yankovoy@marvel.ru>
2024-02-19 18:22:10 +0300

    Finished lesson 12

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -4,15 +4,22 @@ import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Link from 'next/link';
 import { generatePagination } from '@/app/lib/utils';
+import { usePathname, useSearchParams } from 'next/navigation';
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
-  // NOTE: comment in this code when you get to this point in the course
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const currentPage = Number(searchParams.get('page')) || 1;
 
-  // const allPages = generatePagination(currentPage, totalPages);
+  const createPageURL = (pageNumber: number | string) => {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', pageNumber.toString());
+    return `${ pathname }?${ params.toString() }`;
+  };
 
   return (
     <>
-      {/* NOTE: comment in this code when you get to this point in the course */}
+      {/* NOTE: comment in this code when you get to this point in the course */ }
 
       {/* <div className="inline-flex">
         <PaginationArrow
@@ -47,7 +54,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
           href={createPageURL(currentPage + 1)}
           isDisabled={currentPage >= totalPages}
         />
-      </div> */}
+      </div> */ }
     </>
   );
 }
@@ -75,10 +82,10 @@ function PaginationNumber({
   );
 
   return isActive || position === 'middle' ? (
-    <div className={className}>{page}</div>
+    <div className={ className }>{ page }</div>
   ) : (
-    <Link href={href} className={className}>
-      {page}
+    <Link href={ href } className={ className }>
+      { page }
     </Link>
   );
 }
@@ -110,10 +117,10 @@ function PaginationArrow({
     );
 
   return isDisabled ? (
-    <div className={className}>{icon}</div>
+    <div className={ className }>{ icon }</div>
   ) : (
-    <Link className={className} href={href}>
-      {icon}
+    <Link className={ className } href={ href }>
+      { icon }
     </Link>
   );
 }
